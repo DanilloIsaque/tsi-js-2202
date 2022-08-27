f7d75d7a97edc25b4151a883cd7d46df917a181d
DANILLO ISAQUE BRITO LIMA <danillo.iblima@senacsp.edu.br>
2022-08-26 22:12:33 -0300

    condicionais , repetição , operadores e ternario

---
+++ aquecimento.js
@@ -0,0 +1,87 @@
+//for
+/*
+for(let cont=1;cont<=10;cont++)
+{
+    document.write("<br> numero da linha"+cont);
+}
+
+//while
+let u =1;
+while(u<=10)
+{
+    document.write("<br><br> numero da linha"+ u);
+    u++;
+}
+
+//do
+
+let l=1;
+
+do{
+   document.write("<br> o numero da linha é "+ l +"<br>");
+   l++; 
+}while(l<=10)
+
+//////////
+//conditions
+*/
+//if
+/*
+let x=prompt(parseInt("digite um numero"));
+let y=prompt(parseInt("digite um numero"));;
+if(x>y){
+    document.write(x+" é maior q "+y);
+}
+else
+{
+    if(x==y)
+    {
+        document.write(" são iguais ");
+    }
+    
+    else{
+        document.write(y+" é maior q "+x);
+    }
+    
+    
+}
+*/
+/*
+let x=prompt(parseInt("digite um numero"));
+
+switch(x){
+    case "1":
+        document.write("segunda");
+        break;
+        case "2":
+            document.write("terça");
+            break;
+            case "3":
+                document.write("quarta");
+                break;
+                case "4":
+                    document.write("quinta");
+                    break;
+                    case "5":
+                        document.write("sexta");
+                        break;
+                        case "6":
+                            document.write("sabado");
+                            break;
+
+
+default:
+
+    document.write("animal");
+
+}
+*/
+//ternário
+/*
+let g=prompt("diga 'm' para masculino e f para ' feminino' ");
+
+let resultado = g=="m" ?  ' é masculino' :  ' feminino';
+
+document.write(resultado);
+*/
+

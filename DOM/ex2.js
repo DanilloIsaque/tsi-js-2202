1b792bac9fee6586e7ab0fd3dc8b5eecaaec2a2b
DANILLO ISAQUE BRITO LIMA <danillo.iblima@senacsp.edu.br>
2022-09-16 22:10:32 -0300

    coletar recursos do site e mais coisa importante do DOM

--- DOM/ex2.js
+++ DOM/ex2.js
@@ -1,8 +1,38 @@
 let btnCalcular=document.getElementById('calcular');
 btnCalcular.style.background="red";
-
+//quando alguem clicar no botao, executa a função q pega o getElementById e joga em
+//serv
 btnCalcular.addEventListener('click',function(evento){
     evento.preventDefault();
-    console.log(evento);
-})
 
+    let serv= document.getElementById('servico');
+    let prazo=document.getElementById('prazo');
+    let res= document.getElementById('resultado');
+    //recuperar os dados da matriz
+    
+    let preco= calculaPreco(serv.value,prazo.value);
+   
+
+    
+    
+    
+    
+
+    res.innerHTML=`o preco é ${preco}`;
+    //mostra o que foi escrito, no caso 1,2,3
+    console.log(serv.value);
+
+    //ver dados do evento(click) no console
+    console.log(evento);
+
+    //obter os valores de serviço e prazo
+});
+
+function calculaPreco(serv,prazo)
+{
+    let tabela = [["$$$","$$","$"],//troca de oleo
+    ["$$$$" ,"$$$","$$"],//balanceamento
+    ["nao tem","nao tem","$"]];//cambagem
+
+ return tabela[serv][prazo];
+}
---
+++ ex3.js
@@ -0,0 +1,21 @@
+let elemento;
+//let elemento=document;
+
+//elemento = elemento.head;
+//elemento=document.body;
+
+//chama o primeiro elemento filho
+//elemento=document.body.firstElementChild;
+//elemento=document.body.firstElementChild.firstElementChild;
+
+//elemento= document.doctype.name;
+//elemento=document.forms;
+//elemento=document.forms[0];
+//elemento=document.forms[0][1];
+//elemento=document.scripts;
+elemento=document.textContent;
+elemento=document.cookie;
+elemento=document.visibilityState;
+elemento=document.location;
+//elemento=document.scripts[2].getAttribute('src');
+console.log(elemento);

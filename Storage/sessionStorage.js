9c40e6113d2d48869e92535bb3719f190e47b52a
DANILLO ISAQUE BRITO LIMA <danillo.iblima@senacsp.edu.br>
2022-10-14 22:13:06 -0300

    add e apagar na lista de tarefas e sessionStorage

--- Storage/sessionStorage.js
+++ Storage/sessionStorage.js
@@ -1,29 +1,81 @@
-sessionStorage.setItem('nome','Camila');
-sessionStorage.setItem('sobrenome','Silva');
+//como trabalhar com session storage
 
-let btn= document.getElementById('botao');
-btn.addEventListener('click' , mostrar);
+sessionStorage.setItem('nome','Danillo');
+sessionStorage.setItem('sobrenome','Isaque');
+
+let btn = document.getElementById('botao');
+
+btn.addEventListener('click',mostrar);
 
 function mostrar(evento){
+
     evento.preventDefault();
 
-    let entrada=document.getElementById('entrada');
-    let mostrar=  document.getElementById('mostrar');
-    let valor=entrada.value;
-    mostrar.innerText=valor;
-    entrada.value='';
+    let valor = document.getElementById('entrada').value;
+    document.getElementById('mostrar').innerText =
+     valor;
+    document.getElementById('entrada').value= '';
 
-    //após obtermos o dado do usuário
-    //vamos guardá-lo no local storage
-    sessionStorage.setItem('dadoDoUsuario', valor);
-
-
+    //apos obtermos o dado do usuario 
+    //vamos guarda-lo no session storage
+    sessionStorage.setItem('dadoDoUsuario',valor);
 }
-//para recuperar o dado do local storage
-// usamos o localStorage.getItem()
-document.querySelector('#mostrar').value= 
+
+//para recuperar o dado do session storage
+//usamos o sessionStorage.getItem()
+//get item serve p colocar na tela
+//set item inserir o dado no banco
+document.querySelector('#mostrar').innerText = 
 sessionStorage.getItem('dadoDoUsuario');
 
-//e para apagar no local storage?
-//removeItem()
-//sessionStorage.removeItem('dadoDoUsuario');
+// p/ apagar 
+//sessionStorage.removeItem();
+
+// session 
+// x 
+//local
+//qual a diferença entre eles
+//Utilize localStorage para uso a longo prazo e sessionStorage 
+//quando você precisa armazenar algo temporário
+//(ie dados de uma sessão). Ambos também são escopo por fabricantes de navegadores.
+
+
+
+
+
+let vetor=['pera','banana','manga'];
+
+//para guardar algo que não seja uma string
+//no localstorage ou sessionStorage, temos que converter
+//para string.com o metodo Json.stringify() transformamos
+//vetores e objetos em strings
+
+
+sessionStorage.setItem('frutas',
+                        JSON.stringify(vetor));
+
+//let recuperaDado=sessionStorage.getItem('frutas')//guarda no banco
+
+//para recuperar uma string JSON do 
+//localstorage ou sessionStorage e 
+//transformar em um objeto ou vetor novamente
+//use JSON.parse()
+let recupDado=JSON.parse(
+                    sessionStorage.
+                            getItem('frutas'));//JSON transforma vetor para string
+
+
+//alterações no vetor recuperadi
+//do sessionStorage
+recupDado[3]='morango';
+recupDado[1]='parchita';
+console.log(recupDado);
+
+
+let strRecupDados=JSON.stringify(recupDado);
+
+console.log(strRecupDados);
+
+//atualiza o item fruta no sessionStorage
+sessionStorage.setItem('frutas',strRecupDados);
+//console.log(recupera[10]);
